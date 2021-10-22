7f0760210760fcb6b0fe0c826ad6b1e82dabebdb
erictomson <erictomson@gmail.com>
2021-10-22 10:14:02 -0300

    Criação do projeto de testes unitários.

---
+++ src/test/java/br/com/DHTEST/HelloTest.java
@@ -0,0 +1,22 @@
+package br.com.DHTEST;
+
+import org.junit.Test;
+
+public class HelloTest {
+
+    // Definindo o método como uma aplicação
+    // de teste unitário JUnit
+    @Test
+    public void estrutura() {
+
+        // Cenário de teste
+
+
+        // Execução do teste
+
+
+        // Verificação do teste
+
+    }
+
+}
